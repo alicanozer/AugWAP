38ff2e8aa9f915a3fd5b5344f27e6b6295a6a312
alicanozer <alicanozer60@gmail.com>
2019-08-06 10:45:28 -0500

    week3 day1 assignment is added.

---
+++ pages/maze.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    "use strict";
+    let start = false;
+
+    $("#start").click(function () {
+        start = true;
+        $("#status").html("Started, go on...");
+    });
+
+    $("#end").mouseover(function () {
+        if (start === true) {
+            $("#status").html("You win :)");
+            alert("You win");
+            start = false;
+        }
+    });
+
+    $("div#maze .boundary").each(function (idx, e) {
+        $(e).mouseover(function () {
+            if (start === true) {
+                $("div#maze .boundary").each(function (idx, el) {
+                    $(el).addClass("youlose");
+                });
+                $("#status").html("You lost :(");
+                alert("You lost");
+                start = false;
+            }
+        });
+    });
+});
